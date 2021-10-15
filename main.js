95b478730ebf99cc0b75dba6ffc8d1df07ff34e3
Sergey Peshkov <zloi.g1rz@gmail.com>
2021-10-16 01:01:23 +0800

    Приводим код в порядок

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const $arenas = document.querySelector('.arenas');
+
 const player1 = {
     name: 'Scorpion',
     hp: 90,
@@ -24,26 +26,28 @@ const player2 = {
     }
 };
 
+function createElement(tag, className) {
+    const $tag = document.createElement(tag);
+
+    if (className) {
+        $tag.classList.add(className);
+    }
+
+    return $tag;
+}
+
 function createPlayer(playerClass, playerObj) {
 
-    const $player = document.createElement('div');
-    $player.classList.add(playerClass);
+    const $player = createElement('div', playerClass);
+    const $progressbar = createElement('div', 'progressbar');
+    const $character = createElement('div', 'character');
+    const $life = createElement('div', 'life');
+    const $name = createElement('div', 'name');
+    const $img = createElement('img');
+    
 
-    const $progressbar = document.createElement('div');
-    $progressbar.classList.add('progressbar');
-
-    const $character = document.createElement('div');
-    $character.classList.add('character');
-
-    const $life = document.createElement('div');
-    $life.classList.add('life');
     $life.style.width = playerObj.hp + '%';
-
-    const $name = document.createElement('div');
-    $name.classList.add('name');
     $name.innerText = playerObj.name;
-
-    const $img = document.createElement('img');
     $img.src = playerObj.img;
 
     $progressbar.appendChild($life);
@@ -57,8 +61,6 @@ function createPlayer(playerClass, playerObj) {
     return $player;
 }
 
-
-const $arenas = document.querySelector('.arenas');
 
 $arenas.appendChild(createPlayer('player1', player1));
 $arenas.appendChild(createPlayer('player2', player2));
