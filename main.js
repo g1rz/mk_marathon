a7038cdd7462e8012d4efd953f82e8c1748065e8
Sergey Peshkov <zloi.g1rz@gmail.com>
2021-10-24 15:02:33 +0800

    hw-3

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const $arenas = document.querySelector('.arenas');
+const $randomButton = document.querySelector('.button');
 
 const player1 = {
     player: 1,
     name: 'Scorpion',
-    hp: 90,
+    hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
     weapon: [
         'Fist',
@@ -17,7 +18,7 @@ const player1 = {
 const player2 = {
     player: 2,
     name: 'Sub-zero',
-    hp: 60,
+    hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
     weapon: [
         'Fist',
@@ -62,6 +63,56 @@ function createPlayer(playerObj) {
 
     return $player;
 }
+
+function changeHp(player) {
+    const playerLife = document.querySelector('.player' + player.player +' .life');
+    const hit = getRandomInt(1, 20);
+
+    player.hp = player.hp - hit <= 0 ? 0 : player.hp - hit;
+    // player.hp -= 20;
+    playerLife.style.width = player.hp + '%';
+    console.log(player.hp);
+
+    if (player.hp <= 0) {
+        // $arenas.appendChild(playerLose(player.name))
+
+        const $names = Array.from($arenas.querySelectorAll('.progressbar .name'));
+        let winName = '';
+        $names.map(item => {
+            if ( item.innerText.toLowerCase() !== player.name.toLowerCase() ) {
+                winName = item.innerText;
+            }
+        });
+
+        $arenas.appendChild(playerWins(winName));
+        $randomButton.disabled = true;
+    }
+}
+
+function playerLose(name) {
+    const $loseTitle = createElement('div', 'loseTitle');
+    $loseTitle.innerText = name + ' lose';
+
+    return $loseTitle;
+}
+
+function playerWins(name) {
+    const $winsTitle = createElement('div', 'loseTitle');
+    $winsTitle.innerText = name + ' wins!';
+
+    return $winsTitle;
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(min + Math.random() * ( max + 1 - min ));
+}
+
+$randomButton.addEventListener('click', function() {
+    console.log('Random!');
+
+    changeHp(player1);
+    changeHp(player2);    
+})
 
 
 $arenas.appendChild(createPlayer(player1));
